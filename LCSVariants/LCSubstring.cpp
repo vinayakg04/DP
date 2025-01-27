7b546afa4b1b3422ffa6f6ace52d6763c2ba1dad
Vinayak Gupta <vinayakg1256@gmail.com>
2025-01-27 18:14:40 +0530

    Update LCSubstring.cpp

--- LCSVariants/LCSubstring.cpp
+++ LCSVariants/LCSubstring.cpp
@@ -1,36 +1,24 @@
-class Solution {
-public:
-    int t[1001][1001];
-    int solve(int m,int n,string &s1, string& s2){
-        if(m==0 || n==0) return 0;
-
-        if(t[m][n]!=-1){
-            return t[m][n];
-        }
-
-        if(s1[m-1]==s2[n-1]){
-            return t[m][n]=1+solve(m-1,n-1,s1,s2);
-        }else{
-            return t[m][n]=max(solve(m-1,n,s1,s2),solve(m,n-1,s1,s2));
-        }
-    }
-    int longestCommonSubsequence(string s1, string s2) {
+   int t[1001][1001];
+    int longestCommonSubstr(string& s1, string& s2) {
+        // your code here
         int m=s1.size();
         int n=s2.size();
-
+        
+        int result=0;
+    
         memset(t,0,sizeof(t));
-        // return solve(m,n,s1,s2);
+        
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(s1[i-1]==s2[j-1]){
                     t[i][j]=1+t[i-1][j-1];
+                    result=max(result,t[i][j]);
                 }
                 else{
-                    t[i][j]=max(t[i-1][j],t[i][j-1]);
+                    t[i][j]=0;
                 }
             }
         }
-
-        return t[m][n];
+        
+        return result;
     }
-};
