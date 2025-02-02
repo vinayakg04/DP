4584f41fa1d2be7b8f72b242264117d72e648eda
Vinayak Gupta <vinayakg1256@gmail.com>
2025-02-02 22:55:59 +0530

    Update Check_Panagram.cpp

--- Strings/Check_Panagram.cpp
+++ Strings/Check_Panagram.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+         int l=sentence.size();
+         if(l<26){
+            return false;
+         }
 
+         bool flag=false;
+
+        set<char> st;
+
+        for(char c: sentence){
+            st.insert(c);
+        }
+
+        return st.size()==26;
+        
+
+    }
+};
