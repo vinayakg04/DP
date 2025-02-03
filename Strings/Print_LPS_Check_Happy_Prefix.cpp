ad70fd9c3459da9116981e4aa15ee4d049d9929f
Vinayak Gupta <vinayakg1256@gmail.com>
2025-02-03 22:39:05 +0530

    Update Print_LPS_Check_Happy_Prefix.cpp

--- Strings/Print_LPS_Check_Happy_Prefix.cpp
+++ Strings/Print_LPS_Check_Happy_Prefix.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    string longestPrefix(string s) {
+        string res;
+        int n=s.size();
+        vector<int> LPS(n,0);
+        LPS[0]=0;
 
+        int i=1;
+        int l=0;
+
+        while(i<n){
+            if(s[i]==s[l]){
+                // res+=s[l];
+                l++;
+                LPS[i]=l;
+                i++;
+                
+            }
+            else{
+                if(l!=0){
+                    l=LPS[l-1];
+                }else{
+                    LPS[i]=0;
+                    i++;
+                }
+            }
+        }
+
+        return s.substr(0,LPS[n-1]);
+
+    }
+};
